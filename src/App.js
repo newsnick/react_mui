dc64614341e27a0d691123c2294b6440fe283321
Antonio Lega <atlcontact8@gmail.com>
2023-04-06 12:12:25 +0200

    set dependencies, sass structure, utils.js, pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './styles/App.scss'
-import MenuBar from './UI/MenuBar/MenuBar'
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <MenuBar />
+      <Home />
     </div>
   )
 }
